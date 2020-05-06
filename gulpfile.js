bb9de79d723988051919b051b4533394d834cdfb
Sasha <oleksandr.zoria@masterofcode.com>
2020-05-06 09:10:10 +0300

    sass compile

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+	return gulp.src('app/scss/**/*.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('app/css'))
+})

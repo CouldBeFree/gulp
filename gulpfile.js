bf6ee9110d9b59eee06b6c95a096122b0e14ce8c
Sasha <oleksandr.zoria@masterofcode.com>
2020-05-09 16:08:34 +0300

    js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
+const gulp        = require('gulp');
+const sass        = require('gulp-sass');
 const browserSync = require('browser-sync');
+const uglify      = require('gulp-uglify');
+const concat      = require('gulp-concat');
 
 gulp.task('sass', function () {
 	return gulp.src('app/scss/**/*.scss')
@@ -9,15 +11,34 @@ gulp.task('sass', function () {
 		.pipe(browserSync.reload({stream:true}));
 })
 
+gulp.task('html', function () {
+	return gulp.src('app/*.html')
+		.pipe(browserSync.reload({stream:true}))
+})
+
+gulp.task('js', function () {
+	return gulp.src([
+		'node_modules/slick-carousel/slick/slick.js',
+		'node_modules/magnific-popup/dist/jquery.magnific-popup.js'
+	])
+		.pipe(concat('libs.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('app/js'))
+		.pipe(browserSync.reload({stream: true}))
+})
+
 gulp.task('watch', function () {
-	gulp.watch('app/scss/**/*.scss', gulp.parallel('sass'))
+	gulp.watch('app/scss/**/*.scss', gulp.parallel('sass'));
+	gulp.watch('app/*.html', gulp.parallel('html'));
+	gulp.watch('app/js/*.js', gulp.parallel('js'))
 });
 
 gulp.task('browser-sync', function() {
 	browserSync.init({
 		server: {
 			baseDir: "app/"
-		}
+		},
+		notify: false
 	});
 });
 

f1d73de9c850fe8ebf3e74025e40d55507da7d52
Sasha <oleksandr.zoria@masterofcode.com>
2020-05-09 15:26:29 +0300

    browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,24 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const browserSync = require('browser-sync');
 
 gulp.task('sass', function () {
 	return gulp.src('app/scss/**/*.scss')
-		.pipe(sass())
+		.pipe(sass({outputStyle: 'expanded'}))
 		.pipe(gulp.dest('app/css'))
+		.pipe(browserSync.reload({stream:true}));
 })
+
+gulp.task('watch', function () {
+	gulp.watch('app/scss/**/*.scss', gulp.parallel('sass'))
+});
+
+gulp.task('browser-sync', function() {
+	browserSync.init({
+		server: {
+			baseDir: "app/"
+		}
+	});
+});
+
+gulp.task('default', gulp.parallel('browser-sync', 'watch'));
